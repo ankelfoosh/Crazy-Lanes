26e28c3b17ca2fce3cfff6f4e76d2358654e5ed8
ankelfoosh <leif@otis8.com>
2026-08-11 16:44:12 -0500

    Updated Movement
    I changed the movement a bit to work with camera movement.

--- Crazy Lanes Game/Crazy Lanes/Assets/Scripts/PlayerMovement.cs
+++ Crazy Lanes Game/Crazy Lanes/Assets/Scripts/PlayerMovement.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody rb;
     private bool touchGround;
-    [SerializeField] private Vector3 groundDetector;
+    [SerializeField] private Transform groundDetector;
     
     // -------Movement Vars-------
 
@@ -18,20 +19,35 @@ public class PlayerMovement : MonoBehaviour
     private float xTime;
     private float zVel;
     private float zTime;
+    private float sVel;
+    private float sTime;
     [SerializeField] private float stopDelta;
     
     // -------Jumping-------
 
     [SerializeField] private float initialJumpVel;
     [SerializeField] private float inAirMovementMult;
+
+    private float groundCheckBoxSize = 0.3f;
+    private float groundCheckBoxHeight = 0.05f;
+    
+    // -------Camera Movement-------
+
+    private Vector2 prevMousePos;
+    [SerializeField] private float sensitivity;
     
     // -------Init-------
     
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        touchGround = Physics.CheckBox(groundDetector, groundDetector, 
+
+        groundDetector = GameObject.Find("Player/Ground Check").GetComponent<Transform>();
+        touchGround = Physics.CheckBox(groundDetector.position, 
+            new Vector3(groundCheckBoxSize, groundCheckBoxHeight, groundCheckBoxSize), 
             Quaternion.Euler(0f, 0f, 0f), LayerMask.GetMask("Ground"));
+        
+        prevMousePos = Input.mousePosition;
     }
     
     // -------Jump Functions-------
@@ -45,46 +61,82 @@ public class PlayerMovement : MonoBehaviour
     private void Jump()
     {
         rb.AddForce(new Vector3(rb.linearVelocity.x, initialJumpVel, rb.linearVelocity.z), ForceMode.Impulse);
+        Debug.Log("jump");
     }
     
     // -------Movement Functions-------
 
     void FixedUpdate()
     {
-        touchGround = Physics.CheckBox(groundDetector, groundDetector, 
+        touchGround = Physics.CheckBox(groundDetector.position, 
+            new Vector3(groundCheckBoxSize, groundCheckBoxHeight, groundCheckBoxSize), 
             Quaternion.Euler(0f, 0f, 0f), LayerMask.GetMask("Ground"));
         
         if (canMove)
         {
             RunMovement(touchGround ? 1f : inAirMovementMult);
-            rb.linearVelocity = new Vector3(Mathf.Clamp(xVel,-maxSpeed, maxSpeed), rb.linearVelocity.y, 
-                Mathf.Clamp(zVel,-maxSpeed, maxSpeed));
+            rb.linearVelocity = new Vector3(Mathf.Clamp(xVel,-maxSpeed, maxSpeed) * Mathf.Sin(Mathf.Deg2Rad * transform.rotation.eulerAngles.y), rb.linearVelocity.y, 
+                Mathf.Clamp(zVel,-maxSpeed, maxSpeed) * Mathf.Cos(Mathf.Deg2Rad * transform.rotation.eulerAngles.y));
         }
+        
+        // Camera
+
+        Vector2 mouseDelta = (Vector2) Input.mousePosition - prevMousePos;
+        prevMousePos = Input.mousePosition;
+
+        if (Mathf.Abs(mouseDelta.x) > 0f || Mathf.Abs(mouseDelta.y) > 0f)
+            RotateCamera(mouseDelta.x * sensitivity, mouseDelta.y * sensitivity);
     }
     
-    private void RunMovement(float speedMult)
+    private void RunMovement(float accelMult)
     {
         float xMult = 1f - Mathf.Abs(zTime - 0.5f) / 2;
         float zMult = 1f - Mathf.Abs(xTime - 0.5f) / 2;
+        float sMult = 1f - Mathf.Abs(sTime - 0.5f) / 2;
         xVel = Mathf.Lerp(-maxSpeed * xMult, maxSpeed * xMult, xTime);
         zVel = Mathf.Lerp(-maxSpeed * zMult, maxSpeed * zMult, zTime);
-        
+
         if (Input.GetKey(KeyCode.W))
-            zTime = Mathf.Clamp01(zTime += Time.deltaTime * acceleration);
+        {
+            zTime = Mathf.Clamp01(zTime += Time.deltaTime * acceleration * accelMult);
+            xTime = Mathf.Clamp01(xTime += Time.deltaTime * acceleration * accelMult);
+        }
         else if (Input.GetKey(KeyCode.S))
-            zTime = Mathf.Clamp01(zTime -= Time.deltaTime * acceleration);
+        {
+            zTime = Mathf.Clamp01(zTime -= Time.deltaTime * acceleration * accelMult);
+            xTime = Mathf.Clamp01(xTime -= Time.deltaTime * acceleration * accelMult);
+        }
         else if (Mathf.Abs(zTime - 0.5f) > stopDelta)
-            zTime = Mathf.Clamp01(zTime += Time.deltaTime * decelMultiplier * -Mathf.Sign(zTime - 0.5f));
+        {
+            zTime = Mathf.Clamp01(zTime += Time.deltaTime * decelMultiplier * accelMult * -Mathf.Sign(zTime - 0.5f));
+            xTime = Mathf.Clamp01(xTime += Time.deltaTime * decelMultiplier * accelMult * -Mathf.Sign(xTime - 0.5f));
+        }
         else
+        {
             zTime = 0.5f;
+            xTime = 0.5f;
+        }
 
         if (Input.GetKey(KeyCode.D))
-            xTime = Mathf.Clamp01(xTime += Time.deltaTime * acceleration);
+            sTime = Mathf.Clamp01(sTime += Time.deltaTime * acceleration * accelMult);
         else if (Input.GetKey(KeyCode.A))
-            xTime = Mathf.Clamp01(xTime -= Time.deltaTime * acceleration);
-        else if (Mathf.Abs(xTime - 0.5f) > stopDelta)
-            xTime = Mathf.Clamp01(xTime += Time.deltaTime * decelMultiplier * -Mathf.Sign(xTime - 0.5f));
+            sTime = Mathf.Clamp01(sTime -= Time.deltaTime * acceleration * accelMult);
+        else if (Mathf.Abs(zTime - 0.5f) > stopDelta)
+        {
+            sTime = Mathf.Clamp01(sTime += Time.deltaTime * decelMultiplier * accelMult * -Mathf.Sign(sTime - 0.5f));
+        }
         else
-            xTime = 0.5f;
+        {
+            sTime = 0.5f;
+        }
+    }
+
+    private void RotateCamera(float xAmount, float yAmount)
+    {
+        Transform camera = GameObject.Find("Player/Main Camera").GetComponent<Transform>();
+        
+        transform.Rotate(Vector3.up, xAmount);
+        //camera.RotateAround(transform.position, new Vector3(1 * Mathf.Sign((Mathf.Abs(transform.rotation.eulerAngles.y - 180f) - 90f)), 0, 0), -yAmount);
+        camera.Rotate(new Vector3(1, 0, 0), -yAmount);
     }
 }
